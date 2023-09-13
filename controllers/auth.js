a5a72762408f6d1cd79b2eb97d8193b8e73cfd41
Zuzana Kormancova <zunakorma@gmail.com>
2023-09-13 14:52:29 +0200

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,35 @@
+const User = require("../models/User");
+const {StatusCodes} = require("http-status-codes");
+const {BadRequestError, UnauthenticatedError} = require("../errors");
+
+const register = async (req, res) =>{
+  // using mongoose middleware in models/User .pre(hash password...)
+  // https://mongoosejs.com/docs/middleware.html#pre
+  const user = await User.create({...req.body});
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({user: {name: user.name}, token});
+};
+
+const login = async (req, res) =>{
+  const {email, password} = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({email});
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({user: {name: user.name}, token});
+};
+
+
+module.exports = {
+  register, login,
+};
---
+++ controllers/jobs.js
@@ -0,0 +1,64 @@
+const Job = require("../models/Job");
+const {StatusCodes} = require("http-status-codes");
+const {BadRequestError, NotFoundError} = require("../errors");
+
+
+const getAllJobs = async (req, res) =>{
+  const jobs = await Job.find({createdBy: req.user.userId}).sort("createdAt");
+  res.status(StatusCodes.OK).json({jobs, count: jobs.length});
+};
+
+const getJob = async (req, res) =>{
+  // get all info from req!!!
+  const {user: {userId}, params: {id: jobId}} = req;
+  const job = await Job.findOne({
+    _id: jobId, createdBy: userId,
+  });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId} found`);
+  }
+  res.status(StatusCodes.OK).json({job});
+};
+
+const createJob = async (req, res) =>{
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({job});
+};
+
+const updateJob = async (req, res) =>{
+  // get all info from req!!! deconstructor
+  const {
+    body: {company, position},
+    user: {userId},
+    params: {id: jobId}} = req;
+
+  if (company === " " || position === " ") {
+    throw new BadRequestError("Company or Position can not be empty!");
+  }
+
+  const job = await Job.findOneAndUpdate(
+      {_id: jobId, createdBy: userId},
+      req.body,
+      {new: true, runValidators: true},
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({job});
+};
+
+const deleteJob = async (req, res) =>{
+  const {
+    user: {userId},
+    params: {id: jobId}} = req;
+  const job = await Job.findOneAndRemove({_id: jobId, createdBy: userId});
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId} found`);
+  }
+  res.status(StatusCodes.OK).send();
+};
+
+module.exports = {
+  getAllJobs, getJob, createJob, updateJob, deleteJob,
+};
